5702642931cd56ea7029c8f711d7f862afd0699c
Adeosun Adeniyi <meisniyi@gmail.com>
2017-07-04 18:55:10 +0200

    fix

--- script/deploy-gluster.sh
+++ script/deploy-gluster.sh
@@ -1,3 +1,3 @@
-#!bin/bash
+#!/bin/bash
 docker run -it -d gluster/gluster-centos /bin/bash
 docker ps -a
--- script/install-docker.sh
+++ script/install-docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt-get update
+
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
